d8c76a3fedd38d02b3e78a61019f85ccbd297b4a
Rohit Agarwal <programmingwithrohit@gmail.com>
2022-03-01 00:30:23 +0530

    Added new features

---
+++ src/test/java/com/javamultiplex/util/SwitchExpressionTest.java
@@ -0,0 +1,44 @@
+package com.javamultiplex.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Rohit Agarwal on 01/03/22 12:12 AM
+ * @copyright www.javamultiplex.com
+ */
+public class SwitchExpressionTest {
+
+    @Test
+    public void shouldUseSwitchExpression() {
+        int a = 10;
+        String output = switch (a) {
+            case 1, 2, 3 -> "hello";
+            case 5, 6, 7 -> "world";
+            default -> "fun";
+        };
+        assertEquals("fun", output);
+    }
+
+    @Test
+    public void shouldUseSwitchExpressionWithYield() {
+        int a = 2;
+        String output = switch (a) {
+            case 1, 2, 3 -> {
+                System.out.println("hello");
+                yield "hello";
+            }
+            case 5, 6, 7 -> "world";
+            default -> "fun";
+        };
+        assertEquals("hello", output);
+        int b = 3;
+        int result = switch (b) {
+            case 1, 2, 3 -> 100;
+            case 5, 6, 7 -> 200;
+            default -> 300;
+        };
+        assertEquals(100, result);
+    }
+}
---
+++ src/test/java/com/javamultiplex/util/TryWithResourceTest.java
@@ -0,0 +1,37 @@
+package com.javamultiplex.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Rohit Agarwal on 01/03/22 12:23 AM
+ * @copyright www.javamultiplex.com
+ */
+public class TryWithResourceTest {
+
+    class R implements AutoCloseable {
+
+        @Override
+        public void close() {
+            System.out.println("closing...");
+        }
+    }
+
+    @Test
+    public void shouldUseTryWithResourceWithJava7() {
+        try (R r1 = new R();
+             R r2 = new R()) {
+
+        }
+    }
+
+    @Test
+    public void shouldUseTryWithResourceWithJava9() {
+        R r1 = new R();
+        R r2 = new R();
+        try (r1;r2) {
+
+        }
+    }
+
+
+}
